aa729e5b7d6089b24d2e78ab1da1abbba5cd44e8
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-07-30 03:09:20 +0200

    arrays

--- intro/2_arrays.js
+++ intro/2_arrays.js
@@ -19,7 +19,31 @@ var writers = [
     ["Friedrich", "Engels", "de"],
 ]
 
-// accé aux elements du tableau
+console.log(writers);
+
+// accés aux elements du tableau
 console.log(writers[0]);
 console.log(`${writers[0][0]}, ${writers[0][1]} (${writers[0][2]})`);
 console.assert("Karl, Marx (de)" === `${writers[0][0]}, ${writers[0][1]} (${writers[0][2]})`);
+
+//sauvegarder la valeur du prénom de Marx
+var karl = writers[0][0]
+
+//éditer un élement du tableau, le prénom de Marx
+writers[0][0] = "Karlito"
+
+console.log(`${writers[0][0]}, ${writers[0][1]} (${writers[0][2]})`);
+console.assert("Karlito, Marx (de)" === `${writers[0][0]}, ${writers[0][1]} (${writers[0][2]})`);
+
+console.assert("Karl" === karl);
+
+//remettre la valeur initale du prénom de Marx
+writers[0][0] = karl;
+
+console.assert(`${karl}, Marx (de)` === `${writers[0][0]}, ${writers[0][1]} (${writers[0][2]})`);
+
+
+/**
+ *  Arrays: pop/push/slice/sort/shift/unshift
+ */
+var numbers = [1, 5, 4, 3, 2]
