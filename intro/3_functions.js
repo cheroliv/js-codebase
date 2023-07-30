cd54fd7535f3265d75a21104e1a2af869de59d9e
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-07-30 18:11:42 +0200

    add code to 3_functions.js

--- intro/3_functions.js
+++ intro/3_functions.js
@@ -1,9 +1,26 @@
-function sayHelloWorld() {
-    console.log("Hello world!");
+/**
+ *  Les fonctions
+ */
+
+//function style legacy
+function sayHelloWorldLegacy() {
+    console.log("Hello world legacy!");
 }
+sayHelloWorldLegacy();
+
+function sayHelloLegacy(firstName, lastName, style) {
+    console.log(`Hello ${firstName}, ${lastName} (${style})`);
+}
+sayHelloLegacy("Cher", "Oliv", "legacy");
+
+
+//function style arrow
+const sayHelloWorld = () => console.log("Hello world!");
 sayHelloWorld();
 
-function sayHello(firstName, lastName) {
+const sayHello = (firstName, lastName) =>
     console.log(`Hello ${firstName}, ${lastName}`);
-}
 sayHello("Cher", "Oliv");
+
+
+//
