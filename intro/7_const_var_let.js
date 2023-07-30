78219c5e294ad5f2c84d89527e5a41cdd15c3f8b
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-07-31 00:31:10 +0200

    ajout de commentaire à const var let

--- intro/7_const_var_let.js
+++ intro/7_const_var_let.js
@@ -16,8 +16,33 @@ const varTest = () => {
     console.assert(x === 2);
 }
 varTest();
-//var x est global a tous les fichier js 
+
+console.log("---------");
+
+//var x est global à tous les fichier js 
 //chargés dans la page html
+//var x issue du fichier 1_start.js
+//ligne 46
 console.log(x);
 console.assert(x === 20);
+
+console.log("---------");
+
+//premier let x est confiné au scope de la fonction
+//le let x du if est confiné au scope du if
+const letTest = () => {
+    let x = 1;
+    console.log(x);
+    console.assert(x === 1);
+    if (true) {
+        let x = 2;
+        console.log(x);
+        console.assert(x === 2);
+    }
+    console.log(x);
+    console.assert(x === 1);
+    console.assert(x !== 2);
+}
+letTest();
+
 console.log("---------");
