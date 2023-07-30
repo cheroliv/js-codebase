252b4b0799a79b6f2b4339684108b23f882d9705
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-07-30 17:30:38 +0200

    add script 3_functions.js

---
+++ intro/3_functions.js
@@ -0,0 +1,9 @@
+function sayHelloWorld() {
+    console.log("Hello world!");
+}
+sayHelloWorld();
+
+function sayHello(firstName, lastName) {
+    console.log(`Hello ${firstName}, ${lastName}`);
+}
+sayHello("Cher", "Oliv");
