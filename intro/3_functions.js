0de10218cef111627c160d31562e6f867e81d84b
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-07-31 01:26:08 +0200

    spread operator

--- intro/3_functions.js
+++ intro/3_functions.js
@@ -50,3 +50,32 @@ const displayAuthors = (authorsArray) => authorsArray.forEach(author =>
 displayAuthors(authors);
 
 console.log("---------");
+
+/**
+ * valeur par défaut des parametres d'une fonctions 
+ */
+const add = (a = 0, b = 0) => a + b;
+console.assert(add() === 0)
+console.assert(add(1) === 1)
+console.assert(add(1, 1) === 2)
+
+console.log("---------");
+/**
+ * Opérateur de rest: 
+ * const add = (...operandes) => a + b;
+ * ou avec parametres avec valeur par défaut
+ * const add = (a=0, b=0, ...operandes) => a + b;
+ */
+
+/**
+ * spread opérateur: 
+ * récupère l'ensemble des elements d'une collection, 
+ * dans un array 
+ */
+const user_ages = [25, 56, 12, 58, 41, 62, 26];
+
+const max_user_age = Math.max(...user_ages);
+const min_user_age = Math.min(...user_ages);
+
+console.assert(max_user_age == 62);
+console.assert(min_user_age == 12);
