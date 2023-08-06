8cea4c60cfdf6882b39fa8de1b1feeaa1f4ed748
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-08-06 02:14:12 +0200

    update math_functions.js

---
+++ intro_js/math_functions.js
@@ -0,0 +1,33 @@
+//sum
+const sum = (x, y) => x + y;
+// subtraction
+const minus = (x, y) => x - y;
+// multiplication
+const times = (x, y) => x * y;
+// division
+const div = (x, y) => x / y;
+// remainder: reste de la division euclidiene
+const rem = (x, y) => x % y;
+// log sum
+const sum_log = (x, y) => console.log(x + y);
+// log subtraction
+const minus_log = (x, y) => console.log(x - y);
+// log multiplication
+const times_log = (x, y) => console.log(x * y);
+// log division
+const div_log = (x, y) => console.log(x / y);
+// log remainder
+const rem_log = (x, y) => console.log(x % y);
+
+export {
+    sum,
+    minus,
+    times,
+    div,
+    rem,
+    sum_log,
+    minus_log,
+    times_log,
+    div_log,
+    rem_log
+};
